43aa3f65772b078d3b3e68d1e314af0cb55671e8
Bob Brodt <bbrodt@redhat.com>
2016-01-15 08:34:03 -0700

    Upversion to 1.0.6 for Neon Requires Java 8 now

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn clean install -B -U -fae -e -Dplatform.name=luna -Dmaven.test.skip=true -Dbuild.at.eclipse.org=false
+mvn clean install -B -U -fae -e -Dplatform.name=neon -Dmaven.test.skip=true -Dbuild.at.eclipse.org=false
